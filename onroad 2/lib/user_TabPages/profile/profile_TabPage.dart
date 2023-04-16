f03927ad880cbbc0c23c569fece63e010423118a
Ahmed Reda <allamahmedreda@gmail.com>
2023-04-16 16:56:09 +0200

    تعديل الملف الشخصي

--- onroad 2/lib/user_TabPages/profile/profile_TabPage.dart
+++ onroad 2/lib/user_TabPages/profile/profile_TabPage.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:onroad/authenticatio/user_or_providr.dart';
 import 'package:onroad/mainScreens/main_screens.dart';
-import 'dart:io';
 import 'package:onroad/user_TabPages/profile/editprofile.dart';
 import 'package:onroad/user_TabPages/profile/profile.dart';
 import 'package:onroad/user_TabPages/profile/profile_body.dart';
@@ -19,17 +17,9 @@ class ProfileTabPage extends StatefulWidget {
 class _ProfileTabPageState extends State<ProfileTabPage> {
   String? name;
   String? image;
-  File? _imageFile;
 
   final _mAuth = FirebaseAuth.instance;
 
-  Future<void> _getImage() async {
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
-    setState(() {
-      _imageFile = File(pickedFile!.path);
-    });
-  }
 
 
   @override
@@ -86,12 +76,7 @@ class _ProfileTabPageState extends State<ProfileTabPage> {
       ),
       body: Column(
         children: [
-          Info(
-            name: 'User Name',
-            image: _imageFile != null ? FileImage(_imageFile!) : null,
-            Camera: false,
-            onPress: _getImage,
-          ),
+          const Info(),
           const SizedBox(
             height: 50.0,
           ),
